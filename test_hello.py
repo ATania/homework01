8e43468d041c10e67f348a6b9667ecd93320aa13
JarnoRFB <ruedigerbusche@web.de>
2018-04-11 23:50:21 +0200

    Give stdout back in test
    This prevents problems when the test is run interactively

--- test_hello.py
+++ test_hello.py
@@ -6,7 +6,9 @@ def test_say_hello():
     assert hasattr(hello_world, 'say_hello'), "Your Script must have an 'say_hello'-function!"
 
     mystdout = StringIO()
+    saved_stdout = sys.stdout
     sys.stdout = mystdout
     hello_world.say_hello()
-
+    sys.stdout = saved_stdout
+    
     assert mystdout.getvalue() == 'hello world\n'
